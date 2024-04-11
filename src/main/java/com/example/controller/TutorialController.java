0fb727393c9bcaae700b4053e887aade63e678d4
Beatriz Cruz <mllebia@live.com>
2024-04-11 16:27:40 -0300

    some-begin-files

---
+++ src/main/java/com/example/controller/TutorialController.java
@@ -0,0 +1,40 @@
+package com.example.controller;
+
+import com.example.model.Tutorial;
+import com.example.repository.TutorialRepository;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@CrossOrigin(origins = "https://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class TutorialController {
+    @Autowired
+    TutorialRepository tutorialRepository;
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+        try {
+            List<Tutorial> tutorials = new ArrayList<Tutorial>();
+
+            if(title == null)
+                tutorialRepository.findAll().forEach(tutorials::add);
+            else
+                tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+}
